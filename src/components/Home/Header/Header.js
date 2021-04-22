2d8f276ff782d35c5c0b6de1c2d4b13ff9a4027c
engrshamim <ewushamim.bd@gmail.com>
2021-04-22 12:20:12 +0600

    heder update

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
             <section className="container">
                 <div className="row pt-3 pb-5">
                     <div className="col-md-5 d-flex flex-column justify-content-center align-items-start order-1 order-md-0">
-                        <h1 className="font-weight-bold py-3">Welcome to</h1><h4>Your Trusted Beauty Treatments center.</h4><br/><br/>
-                        <p>Whether you're visiting us on holiday, for a special event or business meeting, looking your best is a breeze with our salon services for women.</p>
+                        <h1 className="font-weight-bold py-3">Welcome to</h1><h4>Your Trusted Computer servicing center.</h4><br/><br/>
+                        <p>We want our customers to be happy and satisfied in our products and in us. So, it is our priority to provide all types of servicing to our customers.</p>
                         <button className="btn btn-primary px-4">Get an Appointment</button>
                     </div>
                     <div className="col-md-7 d-flex align-items-center justify-content-center order-0 order-md-1">
